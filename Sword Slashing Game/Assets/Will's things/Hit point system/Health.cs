adebda93ae4ab0b2a809824c7aaa0e37d728e6d8
JonMallozzi <jonathan.mallozzi@maine.edu>
2020-04-06 05:28:55 -0400

    test

---
+++ Sword Slashing Game/Assets/Will's things/Hit point system/Health.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    public UnityEvent deathEvent;
+    public UnityEvent damageEvent;
+    public UnityEvent healEvent;
+
+    public int maxHealth = 10;
+    public int currentHealth = 10;
+
+    [SerializeField] float invlunerabilityTime = 1;
+    float lastTimeHit;
+    bool dead = false;
+
+    public void ModifyHealth(int amount)
+    {
+        if (dead)
+        {
+            return; //skip if you die
+        }
+        //run events
+        if(amount > 0)
+        {
+            healEvent?.Invoke();
+        }
+        else if (amount < 0)
+        {
+            if(lastTimeHit + invlunerabilityTime < Time.time)
+            {
+                lastTimeHit = Time.time;
+                damageEvent?.Invoke();
+            }
+            else
+            {
+                print("cancelling damage");
+                return;
+            }
+        }
+        //add or subtract health
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        if(currentHealth == 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        //remove character
+        dead = true;
+        if(deathEvent != null)
+        {
+            deathEvent?.Invoke();
+        }
+        else
+        {
+            print("ERROR NO DEATH EFFECT");
+        }
+    }
+
+    void Ping()
+    {
+        Debug.Log("Ping");
+    }
+}
---
+++ Sword Slashing Game/Assets/Will's things/Prefabs/Turret/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed = 1;
+    public int damage = 1;
+
+    private void FixedUpdate()
+    {
+        //move along the y
+        transform.position += transform.up * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>())
+        {
+            other.GetComponent<Health>().ModifyHealth(-damage);// Damages any health component it collides with
+            Remove();
+        }
+
+    }
+
+    private void Remove()
+    {
+        Destroy(this.gameObject);
+    }
+}
